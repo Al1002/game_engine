0b160a57ab0f94152205426e77cfbc043bc2172a
Alex_laptop <aleksandriliev05@gmail.com>
2025-01-04 19:48:00 +0200

    Wip creating a proper standalone cmake package

---
+++ cmake/install_script.cmake
@@ -0,0 +1,27 @@
+install(TARGETS engine
+    EXPORT game_engineTargets
+    LIBRARY DESTINATION lib
+    ARCHIVE DESTINATION lib
+    RUNTIME DESTINATION bin
+    INCLUDES DESTINATION include)
+
+install(DIRECTORY ${CMAKE_SOURCE_DIR}/src/
+    DESTINATION include/game_engine
+    FILES_MATCHING PATTERN "*.h*"
+)
+
+install(EXPORT game_engineTargets
+    FILE game_engineTargets.cmake
+    NAMESPACE game_engine::
+    DESTINATION lib/cmake/game_engine)
+
+include(CMakePackageConfigHelpers)
+write_basic_package_version_file(
+    game_engineConfigVersion.cmake
+    VERSION ${PROJECT_VERSION}
+    COMPATIBILITY AnyNewerVersion)
+
+install(FILES 
+    cmake/game_engineConfig.cmake
+    ${CMAKE_CURRENT_BINARY_DIR}/game_engineConfigVersion.cmake
+    DESTINATION lib/cmake/game_engine)
