efd0dda175aea4997a5703e94abfb9fbce851fcc
Alex_laptop <aleksandriliev05@gmail.com>
2025-01-06 13:28:55 +0200

    Fixed spelling err

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,3 +1,3 @@
 sudo apt update
-sudo apt install libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev libsdl2-mixer-dev libbox2d-dex # libraries
+sudo apt install libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev libsdl2-mixer-dev libbox2d-dev # libraries
 sudo apt install cmake # build tool
